d46ea8aeafc992ffec0ea26f66359dceacc675ad
Tom D'Silva <20383538+TomDSilva@users.noreply.github.com>
2023-05-29 22:33:26 +0100

    https://github.com/TomDSilva/Robocopy-Backup/releases/tag/v1.6

--- Robocopy-Backup.ps1
+++ Robocopy-Backup.ps1
@@ -1,8 +1,8 @@
-﻿#Requires -Version 5.0
+#Requires -Version 5.0
 
 ###############################################################################################################################
 #                                                                                                                             #
-#  A PowerShell script to backup files to a folder with current timestamp using Robocopy.                                     #
+#  Powershell Script to backup files to a folder with current timestamp using Robocopy                                        #
 #                                                                                                                             #
 #  DISCLAIMER: THIS CODE IS PROVIDED FREE OF CHARGE. UNDER NO CIRCUMSTANCES SHALL I HAVE ANY LIABILITY TO YOU FOR ANY LOSS    #
 #  OR DAMAGE OF ANY KIND INCURRED AS A RESULT OF THE USE OF THIS CODE. YOUR USE OF THIS CODE IS SOLELY AT YOUR OWN RISK.      #
@@ -24,57 +24,14 @@
 # 1.5 : 19/07/2022 : The script now changes the power setting so the machine doesnt go to sleep (and changes back after).     #
 #                    Removes desktop.ini file in the root folder that is being backed up (to avoid Windows displaying user    #
 #                    folders as just 'Desktop' or 'Documents' instead of Desktop-2022-07-19_01-19 etc).                       #
-###############################################################################################################################
-
-###############################################################################################################################
-### Adjustable Variables                                                                                                    ###
-###############################################################################################################################
-
-# If backing up from local then just set to '', otherwise input the name of the remote server hosting the source directory.
-# This is used to store credentials for connections correctly via custom functions further below:
-$sourceClient = 'ServerName'
-# The source folders you want to be backed up.
-# Can be local or remote, and can also just be 1 path if that's all you need:
-$sourceDirs = @(
-    '\\ServerName\Share\FolderName1',
-    '\\ServerName\Share\FolderName2',
-    '\\ServerName\Share\FolderName3'
-)
-
-# If backing up to local then just set to '', otherwise input the name of the remote server hosting the destination directory:
-$destinationClient = 'ServerName'
-# The root destination directory. Timestamped folders will appear under here.
-$destinationDirRoot = '\\ServerName\Share\FolderName'
-
-# Set this to just '' if you dont want to exclude any directories
-# If you want to exclude multiple directories then seperate them via commas and single quatations such as below:
-# $excludedDirArray = '\\ServerName\Share\FolderName\ExcludedFolder1' , '\\ServerName\Share\FolderName\ExcludedFolder2'
-[string[]]$excludedDirArray = ''
-
-# Set this to just '' if you dont want to exclude any files.
-# If you want to exclude multiple files then seperate them via commas and single quotations such as below:
-# $excludedFileArray = '*.log' , '*.txt' , '*.xlxs'
-[string[]]$excludedFileArray = ''
-
-# Set this to just '' if you dont want to log
-$logLocation = 'D:\'
-# Retry attempts in case a file is unable to be read:
-$retryAmount = 10
-# Wait time in seconds:
-$waitAmount = 6
-
-# You probably dont wan't to change these:
-# tempLocation needed in case we are running the script from a network share
-# as elevation to local admin will not be able to read this script:
-$tempLocation = 'C:\Temp'
-$dateTime = Get-Date -Format "yy-MM-dd HH-mm-ss"
-
-###############################################################################################################################
-### End of Adjustable Variables                                                                                             ###
-###############################################################################################################################
-
-###############################################################################################################################
-### Main Script - DO NOT CHANGE BELOW HERE                                                                                  ###
+# 1.6 : 29/05/2023 : Added improved comments.                                                                                 #
+#                    Added default log location to the path that the script is running from. Unless that location is the      #
+#                    templocation for the temp script then it logs to the user's desktop.                                     #
+#                    Added a variable to control if the script should try and elevate to run as admin.                        #
+#                    Added the script now detects if it is running as admin and adjusts backup options to use /ZB if so.      #
+#                    Added basic sanity checks for source and destination. Will now popup with an error if not sufficient.    #
+#                    Added runtime banner.                                                                                    #
+#                    Fixed bug when checking if using a temp script.                                                          #
 ###############################################################################################################################
 
 ###############################################################################################################################
@@ -92,28 +49,104 @@ Set-Location $scriptPath
 ### End of Script Location Checker                                                                                          ###
 ###############################################################################################################################
 
-# Full path name to the temp script that we will be copying under:
-[string]$tempScript = "$tempLocation\$($MyInvocation.MyCommand.Name)"
+###############################################################################################################################
+### Adjustable Variables                                                                                                    ###
+###############################################################################################################################
 
-# If we aren't an admin then run these commands:
-If (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+# If backing up from local then this just needs to be set to ''.
+# Otherwise input the hostname of the remote server hosting the source directory (e.g 'ServerName').
+# This is used to store credentials for connections correctly via custom functions further below:
+$sourceClient = ''
+# The source folders you want to be backed up.
+# Can be local or remote, and can also just be 1 path if that's all you need:
+# Local example: 'C:\Users\Tom\Documents\MyStuff'
+# Remote example: '\\ServerName\Share\FolderName'
+$sourceDirs = @(
+    '\\ServerName\Share\FolderName1',
+    '\\ServerName\Share\FolderName2',
+    '\\ServerName\Share\FolderName3'
+)
 
-    # Check if the temp directory exists, if not then creates it
-    if (-not (test-path $tempLocation)) {
-        New-Item $tempLocation -ItemType "directory"
+# If backing up from local then this just needs to be set to ''.
+# Otherwise input the hostname of the remote server hosting the source directory (e.g 'ServerName').
+# This is used to store credentials for connections correctly via custom functions further below:
+$destinationClient = ''
+# The root destination directory. Timestamped folders will appear under here.
+# Local example: 'C:\Users\Tom\My Backups\'
+# Remote example: '\\ServerName\Share\FolderName'
+$destinationDirRoot = '\\ServerName\Share\FolderName'
+
+# Set this to just '' if you dont want to exclude any directories
+# If you want to exclude multiple directories then seperate them via commas and single quatations such as below:
+# $excludedDirArray = '\\ServerName\Share\FolderName\ExcludedFolder1' , '\\ServerName\Share\FolderName\ExcludedFolder2'
+[string[]]$excludedDirArray = ''
+
+# Set this to just '' if you dont want to exclude any files.
+# If you want to exclude multiple files then seperate them via commas and single quotations such as below:
+# $excludedFileArray = '*.log' , '*.txt' , '*.xlxs'
+[string[]]$excludedFileArray = ''
+
+# Set this to just '' if you dont want to log:
+$logLocation = $scriptPath
+# Retry attempts in case a file is unable to be read:
+$retryAmount = 10
+# Wait time in seconds:
+$waitAmount = 6
+
+# You probably dont wan't to change these:
+# If you want to run the script as admin, then set as $true. Robocopy will now use /ZB parameter:
+$runAsAdmin = $false
+# tempLocation needed if we are running as admin as running the script from a network share
+# as elevation to local admin will not be able to read this script:
+$tempLocation = 'C:\Temp'
+# dateTime used for logging purposes:
+$dateTime = Get-Date -Format 'yy-MM-dd HH-mm-ss'
+
+###############################################################################################################################
+### End of Adjustable Variables                                                                                             ###
+###############################################################################################################################
+
+###############################################################################################################################
+### Main Script - DO NOT CHANGE BELOW HERE                                                                                  ###
+###############################################################################################################################
+
+Clear-Host
+Write-Host '===================== Robocopy Mirror ====================='
+Write-Host ''
+Write-Host "By Tom D'Silva 2021 - https://github.com/TomDSilva"
+Write-Host ''
+Write-Host 'DISCLAIMER: THIS CODE IS PROVIDED FREE OF CHARGE.'
+Write-Host 'UNDER NO CIRCUMSTANCES SHALL I HAVE ANY LIABILITY TO YOU FOR ANY LOSS'
+Write-Host 'OR DAMAGE OF ANY KIND INCURRED AS A RESULT OF THE USE OF THIS CODE.'
+Write-Host 'YOUR USE OF THIS CODE IS SOLELY AT YOUR OWN RISK.'
+Write-Host ''
+Write-Host '==========================================================='
+
+# If the script has been set to run as admin then run the enclosed commands:
+if ($runAsAdmin -eq $true) {
+    # Full path name to the temp script that we will be copying under:
+    [string]$tempScript = "$tempLocation\$($MyInvocation.MyCommand.Name)"
+
+    # If we aren't an admin then run these commands:
+    If (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+
+        # Check if the temp directory exists, if not then creates it
+        if (-not (test-path $tempLocation)) {
+            New-Item $tempLocation -ItemType "directory"
+        }
+
+        # Copy our script to a temp location to be run as admin:
+        Copy-Item -Path $MyInvocation.MyCommand.Path -Destination $tempLocation
+
+        # Relaunch this temp script as an elevated process:
+        Start-Process powershell.exe "-File", ('"{0}"' -f $tempScript) -Verb RunAs
+
+        # Exit (this current non admin session)
+        exit
     }
 
-    # Copy our script to a temp location to be run as admin:
-    Copy-Item -Path $MyInvocation.MyCommand.Path -Destination $tempLocation
-
-    # Relaunch this temp script as an elevated process:
-    Start-Process powershell.exe "-File", ('"{0}"' -f $tempScript) -Verb RunAs
-
-    # Exit (this current non admin session)
-    exit
+    # Now running elevated so run the rest of the script:
 }
-
-# Now running elevated so run the rest of the script:
 
 ###############################################################################################################################
 ### Function(s)                                                                                                             ###
@@ -124,6 +157,7 @@ function Assert-OK { if ($LASTEXITCODE -ne 0) { throw } }
 
 function Find-Credential {
     param( [string]$serverName)
+
     [string]$storedCreds = cmdkey.exe "/list:Domain:target=$serverName"
 
     if ($storedCreds -like '* NONE *') {
@@ -133,6 +167,7 @@ function Find-Credential {
 
 function Set-Credential {
     param( [string]$serverName)
+
     $username = Read-Host -Prompt 'Username?'
     $password = Read-Host -Prompt 'Password?' -AsSecureString
     
@@ -150,7 +185,41 @@ function Set-Credential {
 ###############################################################################################################################
 
 ###############################################################################################################################
-### Stop PC Sleeping                                                                                                        ###
+### Start of Error Checking                                                                                                 ###
+###############################################################################################################################
+
+# If source and destination variables hasnt been set then the script cant run.
+# Therefore display popup message and exit script:
+if ('' -eq $sourceDir -or '' -eq $destinationDir) {
+    Add-Type -AssemblyName PresentationCore, PresentationFramework
+    $MessageboxTitle = 'ERROR'
+    $Messageboxbody = '$sourceDir and/or $destinationDir not filled in, please check script user variables.'
+    $ButtonType = [System.Windows.MessageBoxButton]::OK
+    $MessageIcon = [System.Windows.MessageBoxImage]::Error
+    [System.Windows.MessageBox]::Show($Messageboxbody, $MessageboxTitle, $ButtonType, $messageicon)
+    exit
+}
+
+# If the source directory doesnt exist then the script cant run.
+# Therefore display popup message and exit script:
+foreach ($sourceDir in $sourceDirs){
+    if (!(Test-Path $sourceDir)) {
+        Add-Type -AssemblyName PresentationCore, PresentationFramework
+        $MessageboxTitle = 'ERROR'
+        $Messageboxbody = '$sourceDir doesnt exist, please check script user variables.'
+        $ButtonType = [System.Windows.MessageBoxButton]::OK
+        $MessageIcon = [System.Windows.MessageBoxImage]::Error
+        [System.Windows.MessageBox]::Show($Messageboxbody, $MessageboxTitle, $ButtonType, $messageicon)
+        exit
+    }
+}
+
+###############################################################################################################################
+### End of Error Checking                                                                                                   ###
+###############################################################################################################################
+
+###############################################################################################################################
+### Start of Stop PC Sleeping                                                                                               ###
 ###############################################################################################################################
 
 # Define the properties of a custom power scheme, to be created on demand.
@@ -194,6 +263,11 @@ try {
     ### End of Stop PC Sleeping                                                                                                 ###
     ###############################################################################################################################
 
+    # If log location is going to the same place as the temp location then divert it to the users desktop:
+    if ($logLocation -eq $tempLocation) {
+        $logLocation = "$env:USERPROFILE\Desktop"
+    }
+
     # Sanitize string
     if ($logLocation[-1] -eq '\') {
         $logLocation = $logLocation.TrimEnd('\')
@@ -204,10 +278,12 @@ try {
         $destinationDirRoot = $destinationDirRoot.TrimEnd('\')
     }
 
+    # If source client is defined then its a remote system:
     if ($sourceClient -ne '') {
         Find-Credential($sourceClient)
     }
 
+    # If destination client is defined then its a remote system:
     if ($destinationClient -ne '') {
         Find-Credential($destinationClient)
     }
@@ -233,12 +309,21 @@ try {
             New-Item -ItemType Directory -Path $destinationDir
         }
 
-        $robocopyCommand = "Robocopy.exe `"$sourceDir`" `"$destinationDir`" /ZB /MIR /MT /E /R:$retryAmount /W:$waitAmount /TEE /COPY:DT"
+        $robocopyCommand = "Robocopy.exe `"$sourceDir`" `"$destinationDir`""
 
+        # If we are running as an admin then use /ZB mode.
+        If (([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+            $robocopyCommand = $robocopyCommand + ' /ZB'
+        }
+
+        $robocopyCommand = $robocopyCommand + " /MIR /MT /E /R:$retryAmount /W:$waitAmount /TEE /COPY:DT"
+
+        # If logging is turned on.
         if ('' -ne $logLocation) {
             $robocopyCommand = $robocopyCommand + " /LOG:`"$logLocation\Robocopy Log - $folderName - $dateTime.log`""
         }
 
+        # If we are excluding a directory.
         if ('' -ne $excludedDirArray) {
             foreach ($entry in $excludedDirArray) {
                 $excludedDir += "`'$entry`' "
@@ -246,6 +331,7 @@ try {
             $robocopyCommand = $robocopyCommand + " /XD $excludedDir"
         }
 
+        # If we are excluding files.
         if ('' -ne $excludedFileArray) {
             foreach ($entry in $excludedFileArray) {
                 $excludedFile += "`'$entry`' "
@@ -274,7 +360,7 @@ finally {
     powercfg -setactive $prevGuid
 
     # If we are using a temporary script, then remove it
-    if ($scriptPath -eq (Split-Path $MyInvocation.MyCommand.Path)) {
+    if ($tempLocation -eq (Split-Path $MyInvocation.MyCommand.Path)) {
         Remove-Item -Path $tempScript
     }
 
